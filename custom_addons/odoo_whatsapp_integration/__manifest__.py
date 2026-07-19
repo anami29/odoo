bfca9dcbb47c4aa7794c3248353752a036ee2624
anami29 <kavita.nbc.9876@gmail.com>
2026-07-19 11:55:44 +0530

    fix model references and dependencies in whatsapp integration

--- custom_addons/odoo_whatsapp_integration/__manifest__.py
+++ custom_addons/odoo_whatsapp_integration/__manifest__.py
@@ -13,7 +13,7 @@
     'version': '18.0.1.0.0',
     'license': 'LGPL-3',
 
-    'depends': ['base', 'base_setup', 'sale_management', 'web', 'stock', 'purchase', 'account', 'contacts'],
+    'depends': ['base', 'base_setup', 'sale_management', 'web', 'stock', 'purchase', 'account', 'contacts', 'portal'],
 
     'data': [
         'security/ir.model.access.csv',
